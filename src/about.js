b50b650542c38b948e604c18a274ee53446e2c12
Ruchita05 <medisettiruchitasri@gmail.com>
2026-07-24 00:34:29 +0530

    Completed Bootstrap UI Exploration

---
+++ src/about.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import './assets/css/style.css';
+
+import template from './templates/about.hbs?raw';
+
+document.getElementById('app').innerHTML = template;
---
+++ src/contact.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import './assets/css/style.css';
+
+import template from './templates/contact.hbs?raw';
+
+document.getElementById('app').innerHTML = template;
